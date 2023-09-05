9ee0d8c7b42349b7f459590623673f227162848a
Jeffrey Schütt <jeffrey.schuett@gmail.com>
2023-09-05 10:20:37 +0200

    fixed build error

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Meta from "../src/components/meta";
-import Header from "../src/components/header";
 import Footer from "../src/components/footer/footer";
 import {Box} from "@mui/material";
 import Imprint from "../src/components/imprint/imprint";
